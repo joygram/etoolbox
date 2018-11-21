18e22e4efac725a8899514bcc4128cf06619136e
joygram <joygram@gmail.com>
2018-11-22 06:43:53 +0900

    [yotasdk] update

--- app/src/main/java/com/yotadevices/sdk/EpdUpdateParams.java
+++ app/src/main/java/com/yotadevices/sdk/EpdUpdateParams.java
@@ -41,5 +41,62 @@ public final class EpdUpdateParams
 	{
 		throw new RuntimeException("Stub!");
 	}
+//	public com.yotadevices.framework.EpdUpdateParams getFrameworkEpdParams()
+//	{
+//		throw new RuntimeException("Stub!");
+//	}
+	public int getUpdateType()
+{
+	throw new RuntimeException("Stub!");
+}
+	public int getWhiteStretch()
+	{
+		throw new RuntimeException("Stub!");
+	}
+
+	public void setBlackStretch(int brightness)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setContrast(int contrast)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setCustomAnimation(int customAnimation[])
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setDithering(int dithering)
+	{
+		throw new RuntimeException("Stub!");
+	}
+//	public void setEpdParams(com.yotadevices.framework.EpdUpdateParams epdparms)
+//	{
+//		throw new RuntimeException("Stub!");
+//	}
+	public void setEpdParams(com.yotadevices.sdk.EpdUpdateParams epdparms)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setIsRegionalUpdate(boolean isRegionalUpdate)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setSharpening(int sharpening)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setUpdateModeAdaptiveTimeout(int mUpdateModeAdaptiveTimeout)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setUpdateType(int updateType)
+	{
+		throw new RuntimeException("Stub!");
+	}
+	public void setWhiteStretch(int whiteStretch)
+	{
+		throw new RuntimeException("Stub!");
+	}
 
 }
